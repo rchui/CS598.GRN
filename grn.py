3831dd75ed35d36d1f3b162f50a1596c5d037870
Ryan Chui <ryan.w.chui@gmail.com>
2017-10-29 14:41:14 -0500

    First push

---
+++ grn.py
@@ -0,0 +1,16 @@
+""" grn.py
+
+Main body of grn process.
+"""
+
+import argparse
+
+FLAGS = None
+
+def main():
+    """ Main body of the grn process. """
+    
+
+if __name__ == '__main__':
+    PARSER = argparse.ArgumentParser()
+    main()
