940d4a35fd9a7dc413aadc8bf05661df1314b9db
Ryan Chui <ryan.w.chui@gmail.com>
2017-11-11 13:39:34 -0600

    All data has been read in

--- grn.py
+++ grn.py
@@ -7,6 +7,7 @@ import sys
 import csv
 import argparse
 import multiprocessing
+import itertools
 from sklearn import linear_model
 import numpy as np
 
@@ -24,7 +25,8 @@ def get_expression():
     """
     with open(FLAGS.expression) as expression_file:
         tsv_in = csv.reader(expression_file, delimiter='\t')
-        expression_data = np.asarray([np.asarray(row) for row in tsv_in if 'NA' not in row])
+        tsv_in = list(itertools.islice(tsv_in, 100))
+        expression_data = np.asarray([np.asarray([element.replace('NA', '0') for element in row]) for row in tsv_in])
         expression_data = np.delete(expression_data, 0, 0)
         expression_names = np.asarray([element[0] for element in expression_data])
         expression_data = np.delete(expression_data, 0, 1).astype(float)
@@ -41,13 +43,18 @@ def get_outcome():
     """
     with open(FLAGS.outcome) as outcome_file:
         tsv_in = csv.reader(outcome_file, delimiter='\t')
+        outcome_data = np.asarray([row[1] for row in tsv_in][1:])
+        return outcome_data
 
 def main():
     """ Main body of the grn process. """
+    print()
     expression_data, expression_names = get_expression()
+    outcome_data = get_outcome()
 
-    print(expression_names)
-    print(expression_data)
+    print(expression_names, '\n')
+    print(expression_data, '\n')
+    print(outcome_data, '\n')
 
 if __name__ == '__main__':
     PARSER = argparse.ArgumentParser()
