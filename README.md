5849a47da2e3b1dcf3e72e20da6458140273911c
Ryan Chui <ryan.w.chui@gmail.com>
2017-11-11 13:05:57 -0600

    Push for tiffany

--- README.md
+++ README.md
@@ -1,1 +1,3 @@
 # CS598.GRN
+
+python3 grn.py -e data/genomicMatrix.txt -p data/clinical_data.txt
--- grn.py
+++ grn.py
@@ -12,17 +12,33 @@ import numpy as np
 
 FLAGS = None
 
-def main():
-    """ Main body of the grn process. """
+def get_expression():
+    """ Reads in the data from file.
+
+    Args:
+        None
+
+    Returns:
+        expression_data: gene  expression data
+        expression_names: gene expression names
+    """
     with open(FLAGS.expression) as expression_file:
         tsv_in = csv.reader(expression_file, delimiter='\t')
         expression_data = np.asarray([np.asarray(row) for row in tsv_in if 'NA' not in row])
         expression_data = np.delete(expression_data, 0, 0)
         expression_names = np.asarray([element[0] for element in expression_data])
         expression_data = np.delete(expression_data, 0, 1).astype(float)
+    return expression_data, expression_names
 
-        print(expression_names)
-        print(expression_data)
+def get_outcome():
+    """""
+
+def main():
+    """ Main body of the grn process. """
+    expression_data, expression_names = get_expression()
+
+    print(expression_names)
+    print(expression_data)
 
 if __name__ == '__main__':
     PARSER = argparse.ArgumentParser()
